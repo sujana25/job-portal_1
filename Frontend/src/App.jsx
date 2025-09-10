965f990235c6cb54617ebeaa4818eb211e70593e
sujana25 <160763944+sujana25@users.noreply.github.com>
2025-09-10 12:48:27 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,7 +17,7 @@ import AdminJobs from "./components/admincomponent/AdminJobs.jsx";
 import PostJob from "./components/admincomponent/PostJob";
 import Applicants from "./components/admincomponent/Applicants";
 import ProtectedRoute from "./components/admincomponent/ProtectedRoute";
-import Creator from "./components/creator/Creator.jsx";
+
 
 const appRouter = createBrowserRouter([
   { path: "/", element: <Home /> },
@@ -57,10 +57,7 @@ const appRouter = createBrowserRouter([
     path: "/Browse",
     element: <Browse />,
   },
-  {
-    path:"/Creator",
-    element: <Creator/>
-  },
+  
 
   // /admin
   {
